f7521f0db58cb9c057f8cf8b7ec8371ba3a39611
Rustam Azizov <reenuay777@gmail.com>
2019-10-31 17:55:10 +0000

    Add all columns in «customers» change verification

--- verify/customers.sql
+++ verify/customers.sql
@@ -2,6 +2,14 @@
 
 BEGIN;
 
-SELECT id FROM customers WHERE FALSE;
+SELECT
+  id,
+  email,
+  password_hash,
+  card_id,
+  date_created,
+  is_verified_email
+FROM customers
+WHERE FALSE;
 
 ROLLBACK;
