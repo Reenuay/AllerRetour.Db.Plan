4737095dc424a47876ea03326ea3b2720349d2e9
Rustam Azizov <reenuay777@gmail.com>
2019-12-12 17:58:40 +0000

    Make email unique

--- deploy/customers.sql
+++ deploy/customers.sql
@@ -4,7 +4,7 @@ BEGIN;
 
 CREATE TABLE customers (
   id BIGSERIAL PRIMARY KEY,
-  email TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE,
   password_hash TEXT NOT NULL,
   card_id TEXT NOT NULL UNIQUE,
   date_created TIMESTAMPTZ NOT NULL DEFAULT now(),
